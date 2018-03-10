9855275938784ba8b5bfe382b503196d5bdf5a75
ratnani1996 <himanshuratnani1996@gmail.com>
2018-03-11 01:30:05 +0530

    files are getting saved to the database

--- server.js
+++ server.js
@@ -4,10 +4,78 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+//middlewares
+const path = require('path')
+const crypto = require('crypto')
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+const GridFsStorage = require('multer-gridfs-storage');
+const multer = require('multer')
+const Grid = require('gridfs-stream')
 
+//connect to mongodb database
+const mongoose = require('mongoose')
+const url = "mongodb://localhost:27017/MongoFileUpload"
+const conn = mongoose.createConnection(url);
+// Init gfs
+let gfs;
+
+conn.once('open', () => {
+  // Init stream
+  console.log(`Connection to the database up and runnning`)
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection('uploads');
+});
+  
+//configure storage
+  var storage = new GridFsStorage({
+    url: 'mongodb://localhost:27017/MongoFileUpload',
+    file: (req, file) => {
+      return new Promise((resolve, reject) => {
+        crypto.randomBytes(16, (err, buf) => {
+          if (err) {
+            return reject(err);
+          }
+          const filename = buf.toString('hex') + path.extname(file.originalname);
+          const fileInfo = {
+            filename: filename,
+            bucketName: 'uploads'
+          };
+          resolve(fileInfo);
+        });
+      });
+    }
+  });
+  const upload = multer({ storage });
+
+
+//defining routes
+//route for homepage
 app.get('/', (req, res)=>{
     res.render('index');
 })
+
+//handle post request for upload
+//name="file" in HTML
+app.post('/upload', upload.single('file'), (req, res)=>{
+    res.redirect('/');
+})
+
+
+//route to show all the files
+app.get('/files', function(req, res){
+    gfs.files.find().toArray((err, files)=>{
+        if(!files || files.length == 0){
+            return res.status(404).json({err : 'No files present'})
+        }
+        else{
+            return res.json( files);
+        }
+        
+    })
+})
+
 
 app.listen(3000, ()=>{
     console.log(`Listening to port 3000`);
